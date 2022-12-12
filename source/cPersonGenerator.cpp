759df2039103ee93cb705e36ab41f2f60f409d2e
Siraphong <hideyo99@hotmail.com>
2022-12-11 23:49:04 -0500

    add sin gen and some test

--- source/cPersonGenerator.cpp
+++ source/cPersonGenerator.cpp
@@ -8,6 +8,7 @@
 #include <sstream>		// String Stream
 #include <string>
 
+int genSin();
 
 cPersonGenerator::cPersonGenerator()
 {
@@ -119,6 +120,10 @@ cPerson* cPersonGenerator::generateRandomPerson(void)
 	{
 		newPerson->gender = cPerson::NON_BINARY;
 	}
+	if ((rand() % 3) == 1)
+	{
+		newPerson->gender = cPerson::NON_BINARY;
+	}
 
 	//middle name
 	randName = nameData.find(rand() % name_file_line);
@@ -144,56 +149,23 @@ cPerson* cPersonGenerator::generateRandomPerson(void)
 	}
 	newPerson->streetNumber = rand() % 1500;
 	
+	//city
+	// province
 	//post code
 	
 	//SIN
+	newPerson->SIN = genSin();
+
 	return newPerson;
 }
 
 
-//
-//// Here's a simple way to load the comma delimited files:
-//bool readCSVFile(void)
-//{
-//	// Open the file
-//	std::ifstream namesFile("Names_2010Census.csv");
-//	if (!namesFile.is_open())
-//	{
-//		std::cout << "Didn't open file" << std::endl;
-//		return false;
-//	}
-//
-//	// name,rank,count,prop100k,cum_prop100k,pctwhite,pctblack,pctapi,pctaian,pct2prace,pcthispanic
-//	// SMITH,1,2442977,828.19,828.19,70.9,23.11,0.5,0.89,2.19,2.4
-//	//
-//	// - rank is how popular the last name is, like 1st, 2nd, etc.
-//	// - count is how many people have that last name, so 2,442,977 people have last name "Smith"
-//	// - prop100k is the ratio per 100,000 people. Smith is 828.19, 
-//	//            meaning that there's a 828.19 out of 100,000 chance (0.82819% chance)
-//	//            that someone is named "Smith"
-//
-//	std::string theLine;
-//
-//	unsigned int lineCount = 0;
-//	while (std::getline(namesFile, theLine))
-//	{
-//		lineCount++;
-//		std::stringstream ssLine(theLine);
-//
-//		std::string token;
-//		unsigned int tokenCount = 0;
-//		while (std::getline(ssLine, token, ','))
-//		{
-//			if (tokenCount == 0)
-//			{
-//				std::cout << token << std::endl;
-//			}
-//			// Ignore the other parts of the line
-//			tokenCount++;
-//		}
-//	}
-//
-//	std::cout << "Lines read = " << lineCount << std::endl;
-//
-//	return true;
-//}
+int genSin()
+{
+	int g1, g2, g3;
+	g1 = (rand() % 1000)*1000*1000;
+	g2 = (rand() % 1000) * 1000;
+	g3 = rand() % 1000;
+
+	return g1+g2+g3;
+}
--- source/main.cpp
+++ source/main.cpp
@@ -13,9 +13,32 @@ void main()
 	cPersonGenerator* pPersonGen = new cPersonGenerator();
 	std::string error;
 	pPersonGen->LoadCensusFiles(NAME_FILE, LASTNAME_FILE, ADDR_FILE, error);
-	pPersonGen->generateRandomPerson();
+	cPerson* person = new cPerson[10];
+	for (int i = 0; i < 10; i++)
+	{
+		person[i] = *pPersonGen->generateRandomPerson();
+		printf("UniqueUserID: %d, Gender: %s, Name: %s %s, SIN: %d, Street: %s\n",
+			person[i].getSnotifyUniqueUserID(),
+			person[i].getGenderAsString().c_str(),
+			person[i].first.c_str(),
+			person[i].last.c_str(),
+			person[i].SIN,
+			person[i].streetName.c_str());
+	}
+	//pPersonGen->generateRandomPerson();
+
+
 	cMusicGenerator* pMusicGen = new cMusicGenerator();
 	pMusicGen->LoadMusicInformation(BILLBOARD, error);
-	pMusicGen->getRandomSong();
+	cSong* song = new cSong[10];
+	for (int i = 0; i < 10; i++)
+	{
+		song[i] = *pMusicGen->getRandomSong();
+		printf("getUniqueID: %d, Name: %s, Artist: %s\n",
+			song[i].getUniqueID(),
+			song[i].name.c_str(),
+			song[i].artist.c_str());
+	}
+	
 	std::cout << error << std::endl;
 }
